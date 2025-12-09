fa2332de8bae57209c488ebbfaa3d6b6e7d4823b
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-12-09 15:50:25 +0500

    change bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
-
+import myimg from './assets/work.jpg'
 function App() {
   return (
     <main>
     <div className="container">
+      <div className='img-frame'>
+        <img src={myimg}  width={150}  />
+      </div>
+      <a href="#" className='project-btn'>Landing</a>
       
+
     </div>
     </main>
   )
